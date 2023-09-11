82e8c43698e18685693081818d974d6be0407f81
Rorsa-All <my.journey.h@gmail.com>
2023-09-11 10:49:54 +0300

    add csss and chang

--- php/page1.php
+++ php/page1.php
@@ -1,11 +1,16 @@
+
+<?php
+$title = "First page";
+include "header.php" ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/page1.css">
 </head>
 <body>
     
-</body>
-</html>
+<?php include "footer.php" ?>
