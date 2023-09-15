20ff9641d4833c3903dc06e2c825b79915f9399f
armelant <56300747+armelant@users.noreply.github.com>
2023-09-15 03:33:09 +0300

    .
    .

--- php/header.php
+++ php/header.php
@@ -1,1 +1,16 @@
 
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="C:\Users\evgen\OneDrive\Рабочий стол\ezgif.com-webp-to-jpg.jpg" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <h1>Hello</h1>
+        <a href="index.php">Home</a>
+    </header>
+    <main>
