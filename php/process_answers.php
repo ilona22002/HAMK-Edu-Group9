d2d2cfbbc3a891acfe3546cc161538d6f6f10ce0
armelant <56300747+armelant@users.noreply.github.com>
2023-09-29 15:05:16 +0300

    s

--- php/process_answers.php
+++ php/process_answers.php
@@ -1,6 +1,5 @@
 <?php
-session_start(); 
-
+session_start();
 
 $servername = "sql11.freemysqlhosting.net";
 $username = "sql11649135";
@@ -16,22 +15,35 @@ if ($conn->connect_error) {
 $conn->set_charset("utf8");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "<h1>answers:</h1>";
-    echo "<ul>";
+    $sums = array();
+    $counts = array();
 
     foreach ($_POST['answers'] as $question => $answer) {
         $question = mysqli_real_escape_string($conn, $question);
         $answer = mysqli_real_escape_string($conn, $answer);
 
-        echo "<li>question $question: $answer</li>";
+        $group = ceil($question / 5); 
+
+        if (!isset($sums[$group])) {
+            $sums[$group] = 0;
+            $counts[$group] = 0;
+        }
+        $sums[$group] += $answer;
+        $counts[$group]++;
+    }
+
+    echo "<h1>Average:</h1>";
+    echo "<ul>";
+
+    foreach ($sums as $group => $sum) {
+        $average = $sum / $counts[$group];
+        echo "<li>Average " . (($group - 1) * 5 + 1) . " - " . ($group * 5) . ": $average</li>";
     }
 
     echo "</ul>";
 }
 ?>
 
-
-
-<p><a href="page1.php">back</a></p>
+<p><a href="page1.php">Назад</a></p>
 
 <?php include "footer.php" ?>
