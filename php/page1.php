f77e3a333c9ab8577b4aeef953b2d606295848d9
Rorsa-All <my.journey.h@gmail.com>
2023-09-29 11:34:28 +0300

    header and footer

--- php/page1.php
+++ php/page1.php
@@ -5,12 +5,56 @@ include "header.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/page1.css">
-</head>
-<body>
-    
+<div class="container">
+    <div class="column">
+        <h2>Learnwell English</h2>
+        <?php
+        $servername = "sql11.freemysqlhosting.net";
+        $username = "sql11649135";
+        $password = "YHHTfDSg5T";
+        $dbname = "sql11649135";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection error " . $conn->connect_error);
+        }
+
+        $conn->set_charset("utf8");
+
+        $sql = "SELECT `question` FROM `TABLE 3`"; 
+
+        $result = $conn->query($sql);
+
+        if ($result) {
+            if ($result->num_rows > 0) {
+                $firstRowSkipped = false; 
+                while ($row = $result->fetch_assoc()) {
+                    if ($firstRowSkipped) { 
+                        echo "<div class='item'>";
+                        echo "<div class='column2'>"; 
+                        for ($i = 1; $i <= 5; $i++) {
+                            echo "<label for='scale".$row["question"]."_$i'></label>";
+                            echo "<input type='radio' name='answer_".$row["question"]."' value='$i' id='scale".$row["question"]."_$i'>";
+                        }
+                        echo "</div>";
+                        echo "<div class='column1'>"; 
+                        echo "<p class='line'>" . $row["question"] . "</p>";
+                        echo "</div>";
+                        echo "</div>";
+                    } else {
+                        $firstRowSkipped = true;
+                    }
+                }
+            } else {
+                echo "No data";
+            }
+        } else {
+            echo "Error " . $conn->error;
+        }
+        $conn->close();
+        ?>
+
+
+
 <?php include "footer.php" ?>
