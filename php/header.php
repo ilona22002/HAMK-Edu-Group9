e954e31cd0969ef4e0fff72c03a4b55793853060
Rorsa-All <my.journey.h@gmail.com>
2023-09-29 11:36:27 +0300

    header

--- php/header.php
+++ php/header.php
@@ -1,1 +1,71 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="C:\Users\evgen\OneDrive\Рабочий стол\ezgif.com-webp-to-jpg.jpg" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <h1>Group 9</h1>
+        <header>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarExample01"
+        aria-controls="navbarExample01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarExample01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">STUDENTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">APPLICANTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT US</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+  <!-- Background image -->
+  <div
+    class="p-5 text-center bg-image"
+    style="
+      background-image: url('https://www.hamk.fi/wp-content/uploads/2023/08/hamk-etusivu.jpg');
+      height: 500px;
+    "
+  >
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">WELCOME TO HAMK</h1>
+          <h4 class="mb-3">Häme University of Applied Sciences</h4>
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+          >STUDY OPTIONS</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Background image -->
+</header>
+    </header>
+    <main>
+
