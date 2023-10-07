0fdf2a4b6b3b08a2308930817be765360744ab39
ilona22002 <ilonaprzheleskovskaia@gmail.com>
2023-10-07 15:01:24 +0300

    header & nav design

--- php/header.php
+++ php/header.php
@@ -14,12 +14,10 @@
 
 </head>
 <body>
-    <header>
-        <h1>Group 9</h1>
-        <header>
+
   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
-    <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg navbar">
+    <div class="container-fluid" style="background-color: #D4DEE5;">
       <button
         class="navbar-toggler"
         type="button"
@@ -34,7 +32,7 @@
       <div class="collapse navbar-collapse" id="navbarExample01">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="#">HOME</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">STUDENTS</a>
@@ -55,22 +53,26 @@
   <div
     class="p-5 text-center bg-image"
     style="
-      background-image: url('https://www.hamk.fi/wp-content/uploads/2023/08/hamk-etusivu.jpg');
+      background-image: url('img/header_background.jpg');
+      opacity: 80%;
+      background-postion: center center;
+      background-size: cover;
       height: 500px;
+      width: 100%;
     "
   >
-    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
       <div class="d-flex justify-content-center align-items-center h-100">
         <div class="text-white">
-          <h1 class="mb-3">WELCOME TO HAMK</h1>
-          <h4 class="mb-3">Häme University of Applied Sciences</h4>
+          <h1 class="mb-3" style="color: #000000; font-family: Arial, Helvetica, sans-serif; font-weight: 600; font-size: 40px; text-shadow: 2px 2px 5px white;">
+          WELCOME TO HAMK EDU</h1>
+          <h4 class="mb-3" style="color: #000000; font-family: Arial, Helvetica, sans-serif; font-weight: 600; font-size: 30px; text-shadow: 2px 2px 5px white;">
+            LearnWell Questionnaire</h4>
           <a class="btn btn-outline-light btn-lg" href="#!" role="button"
           >STUDY OPTIONS</a
           >
         </div>
       </div>
     </div>
-  </div>
   <!-- Background image -->
 </header>
     </header>
